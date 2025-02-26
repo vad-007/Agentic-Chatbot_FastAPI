eb51fdfe32abeadafa7b442240ecd1a67ee2caf0
Deepak Singh <alwaysgrow007@gmail.com>
2025-02-26 17:49:07 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,19 +1,31 @@
-from dotenv import load_dotenv
-load_dotenv()
-import streamlit as st
+# if you dont use pipenv uncomment the following:
+# from dotenv import load_dotenv
+# load_dotenv()
+
 
 #Step1: Setup UI with streamlit (model provider, model, system prompt, web_search, query)
+import streamlit as st
 
-st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
-st.title("AI Chatbot Agents")
-st.write("Create and Interact with the AI Agents!")
+
+# Set page configuration (must be the first Streamlit command)
+st.set_page_config(page_title="AI Chatbot Agents", page_icon="🤖", layout="wide")
+st.write("Welcome to the AI Chatbot Agents app!")
+# Display an image in the sidebar
+st.sidebar.image("AI Chatbot Agent.png", use_container_width=True)
+
+# Additional UI elements
+st.sidebar.header("Navigation")
+st.sidebar.write("Use the sidebar to explore different features.")
+
+st.write("This is an AI-powered chatbot designed to assist with various queries.")
+
 
 system_prompt=st.text_area("Define your AI Agent: ", height=70, placeholder="Type your system prompt here...")
 
 MODEL_NAMES_GROQ = ["llama-3.3-70b-versatile", "mixtral-8x7b-32768"]
 MODEL_NAMES_OPENAI = ["gpt-4o-mini"]
 
-provider=st.radio("Select Provider:", ("Groq", "OpenAI"))
+provider=st.sidebar.radio("Select Provider:", ("Groq", "OpenAI"))
 
 if provider == "Groq":
     selected_model = st.selectbox("Select Groq Model:", MODEL_NAMES_GROQ)
@@ -31,15 +43,16 @@ if st.button("Ask Agent!"):
         #Step2: Connect with backend via URL
         import requests
 
-    payload={
+        payload={
             "model_name": selected_model,
             "model_provider": provider,
             "system_prompt": system_prompt,
             "messages": [user_query],
             "allow_search": allow_web_search
         }
-    response=requests.post(API_URL, json=payload)
-    if response.status_code == 200:
+
+        response=requests.post(API_URL, json=payload)
+        if response.status_code == 200:
             response_data = response.json()
             if "error" in response_data:
                 st.error(response_data["error"])
